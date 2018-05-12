f3ac72a69d45392c1799cdf4fcdeb46a2176e250
Alee14 <Alee14498@gmail.com>
2018-05-12 09:13:32 -0400

    about form

--- ShiftOS Decypter/Form1.vb
+++ ShiftOS Decypter/Form1.vb
@@ -46,6 +46,6 @@
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        MessageBox.Show("ShiftOS Hack Client\nBy: Alee14")
+        About.Show()
     End Sub
 End Class
--- ShiftOS Decypter/My Project/AssemblyInfo.vb
+++ ShiftOS Decypter/My Project/AssemblyInfo.vb
@@ -10,8 +10,8 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyTitle("ShiftOS Hack Client")>
 <Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("ShiftOS Decypter")>
+<Assembly: AssemblyCompany("Alee14")>
+<Assembly: AssemblyProduct("ShiftOS Hack Client")>
 <Assembly: AssemblyCopyright("Copyright ©  2018")>
 <Assembly: AssemblyTrademark("")>
 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("0.0.0.1")>
+<Assembly: AssemblyFileVersion("0.0.0.1")>
