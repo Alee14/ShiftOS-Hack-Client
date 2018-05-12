825f0729dab0a5fbf8c0d10b76d6109eac9b65f3
Alee14 <Alee14498@gmail.com>
2018-05-11 20:29:46 -0400

    Changes in assembly title

--- ShiftOS Decypter/My Project/AssemblyInfo.vb
+++ ShiftOS Decypter/My Project/AssemblyInfo.vb
@@ -8,7 +8,7 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("ShiftOS Decypter")>
+<Assembly: AssemblyTitle("ShiftOS Hack Client")>
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("ShiftOS Decypter")>
